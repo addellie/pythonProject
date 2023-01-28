33467d1b1476946af92589dee86711912fd526b6
addellie <adeli.raimkulova@mail.ru>
2023-01-28 22:51:51 +0600

    first commit

---
+++ start.py
@@ -0,0 +1,13 @@
+print("""
+        ++++++ Добро пожаловать в казино!+++++++
+        +++++Делайте ставку и выбирай число!+++++
+        """)
+
+from logic import start
+from decouple import config
+print(start())
+
+
+
+print("Спасибо за игру!")
+
